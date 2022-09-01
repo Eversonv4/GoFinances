7b40741f0d8f7fc05e1e87ffcb4ed47cd7eedd96
Everson Soares <eversonv4@gmail.com>
2022-09-01 18:08:53 -0300

    project goFinance

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MainTitle, Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <MainTitle>Dashboard</MainTitle>
+    </Container>
+  );
+}
