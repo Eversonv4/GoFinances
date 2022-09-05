571da9e017990f72ee82042e23fc5a73ab30940c
Everson Soares <eversonv4@gmail.com>
2022-09-05 18:39:46 -0300

    adicionando cartões de transações financeiras

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { HighlightCard } from "@components/HighlightCard";
+import { TransactionCard } from "@components/TransactionCard";
 import React from "react";
 import {
   Container,
@@ -8,9 +10,23 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon,
+  HighlightCards,
+  Transactions,
+  Title,
 } from "./styles";
 
 export function Dashboard() {
+  const data = {
+    title: "Desenvolvimento de site",
+    amount: "R$ 12.000,00",
+    category: {
+      name: "Vendas",
+      icon: "dollar-sign",
+    },
+    date: "05/09/2022",
+  };
+
   return (
     <Container>
       <Header>
@@ -22,8 +38,35 @@ export function Dashboard() {
               <UserName>Everson</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
+
+      <HighlightCards>
+        <HighlightCard
+          type="up"
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransation="Última entrada dia 13 de abril"
+        />
+        <HighlightCard
+          type="down"
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransation="Última saída dia 03 de abril"
+        />
+        <HighlightCard
+          type="total"
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransation="01 a 16 de abril"
+        />
+      </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard data={data} />
+      </Transactions>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components/native";
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
+import { Platform } from "react-native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 
 export const Container = styled.View`
   flex: 1;
@@ -12,14 +15,21 @@ export const Header = styled.View`
   background-color: ${({ theme }) => theme.colors.primary};
 
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   flex-direction: row;
 `;
 
 export const UserWrapper = styled.View`
-  background: green;
   width: 100%;
+
   padding: 0 24px;
+  margin-top: ${Platform.OS === "ios"
+    ? getStatusBarHeight() + RFValue(28)
+    : +RFValue(28)}px;
+
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const UserInfo = styled.View`
@@ -45,4 +55,32 @@ export const UserName = styled.Text`
 
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: ${RFValue(24)}px;
+`;
+
+export const HighlightCards = styled.ScrollView.attrs({
+  horizontal: true,
+  showsHorizontalScrollIndicator: false,
+  contentContainerStyle: { paddingHorizontal: 24 },
+})`
+  width: 100%;
+  position: absolute;
+
+  margin-top: ${RFPercentage(20)}px;
+`;
+
+export const Transactions = styled.View`
+  flex: 1%;
+  padding: 0 24px;
+
+  margin-top: ${RFPercentage(14)}px;
+`;
+export const Title = styled.Text`
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.medium};
+  margin-bottom: ${RFValue(16)}px;
 `;
